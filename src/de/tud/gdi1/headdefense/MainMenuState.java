db9499a857aff2338e502f0e8cfb37efaa937957
Phil <p.geiger@stud.tu-darmstadt.de>
2018-11-22 19:02:52 +0100

    filled_mainmenustate-dummy code
    filled the mainmenustate with dummy code

--- src/de/tud/gdi1/headdefense/MainMenuState.java
+++ src/de/tud/gdi1/headdefense/MainMenuState.java
@@ -1,5 +1,34 @@
 package de.tud.gdi1.headdefense;
 
-public class MainMenuState {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
+import eea.engine.entity.StateBasedEntityManager;
+
+public class MainMenuState extends BasicGameState{
+	private int stateID; 							// Identifier von diesem BasicGameState
+	private StateBasedEntityManager entityManager; 	// zugehoeriger entityManager
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
