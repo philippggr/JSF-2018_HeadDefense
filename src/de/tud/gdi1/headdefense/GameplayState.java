1b3b90f6efe66cf100d1e93d62414eb93db441fa
Phil <p.geiger@stud.tu-darmstadt.de>
2018-11-22 19:04:26 +0100

    filled gameplaystate with dummy code
    filled gameplaystate with dummy code

--- src/de/tud/gdi1/headdefense/GameplayState.java
+++ src/de/tud/gdi1/headdefense/GameplayState.java
@@ -1,5 +1,43 @@
 package de.tud.gdi1.headdefense;
 
-public class GameplayState {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
+import eea.engine.entity.StateBasedEntityManager;
+
+public class GameplayState extends BasicGameState{
+	private int stateID; 							// Identifier dieses BasicGameState
+	private StateBasedEntityManager entityManager; 	// zugehoeriger entityManager
+    
+    GameplayState( int sid ) {
+       stateID = sid;
+       entityManager = StateBasedEntityManager.getInstance();
+    }
+
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
